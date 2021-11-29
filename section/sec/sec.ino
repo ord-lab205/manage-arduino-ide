3c2ffce30da4edf967ffa217722c8d89a035363b
Han sanghyun <hshf123@naver.com>
2021-11-29 19:44:55 +0900

    [한상현] 소리센서 정밀도 증가

--- section/sec/sec.ino
+++ section/sec/sec.ino
@@ -1,24 +1,35 @@
+int a, b, c;
+
 void setup()
 {
-  Serial.begin(9600);
-  int a = analogRead(A0);
-  int b = analogRead(A1);
-  int c = analogRead(A2);
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  delay(100);
+  a = analogRead(A0);
+  //b = analogRead(A0);
+ // c = analogRead(A0);
+  b = analogRead(A1);
+  c = analogRead(A2);
+  
   int sensors[3] = {a, b, c};
 
-  for(int sensor = 0; sensor<3; sensor++)
+  /*for (int i = 0; i < 3; ++i)
   {
-    if(sensors[sensor] > 60 || sensors[sensor] == 0)
+    if (sensors[i] > 60 || sensors[i] == 0)
     {
-      // 0으로 뜨는게 센서가 값을 읽지 못한것인지 진짜 감지된 소리가 없는 것인지 확인 필요
-        Serial.print(sensors[sensor]);
-        Serial.print(" ");
+      Serial.print(sensors[i]);
+      Serial.print(" ");
     }
-  }
-  Serial.println();
+    else
+    {
+      Serial.print(sensors[i]);
+      Serial.print(" ");
+    }
+  }*/
+
+  Serial.println((String) a+" "+b+" "+c);
+  
+  delay(200);
 }
